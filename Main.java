66527ccabd25524a564a8caf68f1024f292fca45
SandraWojcik <161924747+SandraWojcik@users.noreply.github.com>
2024-06-01 10:26:48 +0200

    zad2

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -12,6 +13,7 @@ import java.io.File;
 public class Main extends Application {
     private TextField directoryPathField;
     private TextField searchField;
+    private TextArea resultArea;
 
     public static void main(String[] args) {
         launch(args);
@@ -27,15 +29,18 @@ public class Main extends Application {
         searchField = new TextField();
         searchField.setPromptText("Enter search phrase");
 
+        resultArea = new TextArea();
+        resultArea.setPrefHeight(400);
+
         Button browseButton = new Button("Browse");
         browseButton.setOnAction(e -> browseDirectory());
 
         Button searchButton = new Button("Search");
 
         HBox hBox = new HBox(10, directoryPathField, browseButton);
-        VBox vBox = new VBox(10, hBox, searchField, searchButton);
+        VBox vBox = new VBox(10, hBox, searchField, searchButton, resultArea);
 
-        Scene scene = new Scene(vBox, 600, 200);
+        Scene scene = new Scene(vBox, 600, 600);  // Zwiększono wysokość sceny, aby zmieścić TextArea
         primaryStage.setScene(scene);
         primaryStage.show();
     }
